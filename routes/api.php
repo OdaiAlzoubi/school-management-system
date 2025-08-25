225d3d3e42be31c18c81809058a6bbf936d57d51
odaialzoubi <odaialzoubi2011@gmail.com>
2025-08-25 16:43:26 +0300

    Edit route/api

--- routes/api.php
+++ routes/api.php
@@ -12,14 +12,16 @@ Route::post('/register', [RegisterController::class, 'register']);
 Route::post('/login', [LoginController::class, 'login']);
 Route::post('/logout', [LoginController::class, 'logout'])->middleware('auth:sanctum');
 
-Route::group(['middleware' => ['auth:sanctum', 'role.permission:' . RoleEnum::ADMINISTRATOR->value], 'prefix' => 'student'], function () {
-    Route::get('/', [StudentController::class, 'index']);
-    Route::post('/create', [StudentController::class, 'store']);
-    Route::put('/update/{id}', [StudentController::class, 'update']);
-    Route::get('/show/{id}', [StudentController::class, 'show']);
-    Route::delete('/delete/{id}', [StudentController::class, 'destroy']);
-});
-Route::group(['middleware' => ['auth:sanctum', 'role.permission:' . RoleEnum::ADMINISTRATOR->value], 'prefix' => 'guardian'], function () {
-    Route::post('/create', [GuardianController::class, 'create']);
-    Route::put('/update/{student_id}/{guardian_user_id}', [GuardianController::class, 'update']);
+Route::group(['middleware' => ['auth:sanctum', 'role.permission:' . RoleEnum::ADMINISTRATOR->value]], function () {
+    Route::group(['prefix' => 'student'], function () {
+        Route::get('/', [StudentController::class, 'index']);
+        Route::post('/create', [StudentController::class, 'store']);
+        Route::put('/update/{id}', [StudentController::class, 'update']);
+        Route::get('/show/{id}', [StudentController::class, 'show']);
+        Route::delete('/delete/{id}', [StudentController::class, 'destroy']);
+    });
+    Route::group(['prefix' => 'guardian'], function () {
+        Route::post('/create', [GuardianController::class, 'create']);
+        Route::put('/update/{student_id}/{guardian_user_id}', [GuardianController::class, 'update']);
+    });
 });
