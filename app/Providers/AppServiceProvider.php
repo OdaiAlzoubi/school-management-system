0eb55ffa4a078e384bc0d14c0081ccf21e260263
odaialzoubi <odaialzoubi2011@gmail.com>
2025-08-27 09:48:35 +0300

    Register SectionRepositoryInterface binding in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,13 +2,15 @@
 
 namespace App\Providers;
 
+use App\Repositories\GradeRepository;
+use App\Repositories\SectionRepository;
 use App\Repositories\StudentRepository;
 use Illuminate\Support\ServiceProvider;
 use App\Repositories\GuardianRepository;
+use App\Repositories\Interface\GradeRepositoryInterface;
+use App\Repositories\Interface\SectionRepositoryInterface;
 use App\Repositories\Interface\StudentRepositoryInterface;
 use App\Repositories\Interface\GuardianRepositoryInterface;
-use App\Repositories\Interface\GradeRepositoryInterface;
-use App\Repositories\GradeRepository;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,6 +22,7 @@ class AppServiceProvider extends ServiceProvider
         $this->app->bind(StudentRepositoryInterface::class, StudentRepository::class);
         $this->app->bind(GuardianRepositoryInterface::class, GuardianRepository::class);
         $this->app->bind(GradeRepositoryInterface::class, GradeRepository::class);
+        $this->app->bind(SectionRepositoryInterface::class, SectionRepository::class);
     }
 
     /**
