d3edee752918c972a8815ee5cc87a6fa18552a29
odaialzoubi <odaialzoubi2011@gmail.com>
2025-08-28 18:10:36 +0300

    Add enrollments relationship to Grade model

--- app/Models/Grade.php
+++ app/Models/Grade.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 use App\Models\Section;
+use App\Models\Subject;
+use App\Models\Enrollment;
 use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -43,5 +45,10 @@ class Grade extends Model
     public function subjects(): HasMany
     {
         return $this->hasMany(Subject::class);
+    }
+
+    public function enrollments(): HasMany
+    {
+        return $this->hasMany(Enrollment::class);
     }
 }
